72f1a7cd2438bf53cd099abcc112c47072aec8cb
Robson Barbosa <132009913+RBxRobson@users.noreply.github.com>
2023-09-08 10:13:36 -0300

    commit inicial

---
+++ exercicioTS/src/main.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicar = (x, y) => x * y;
+const conta1 = multiplicar(10, 20);
+console.log(conta1);
+const concatenar = (nome) => `Olá ${nome}`;
+const saudação1 = concatenar('Robson');
+console.log(saudação1);
---
+++ exercicioTS/src/main.ts
@@ -0,0 +1,7 @@
+const multiplicar = (x: number, y: number) => x * y;
+const conta1 = multiplicar(10, 20);
+console.log(conta1);
+
+const concatenar = (nome: string) => `Olá ${nome}`;
+const saudação1 = concatenar('Robson')
+console.log(saudação1);
